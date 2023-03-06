6aff3428b3fa615f0bb57427ed0d9d418b7a4c24
xetfy <127065512+xetfy@users.noreply.github.com>
2023-03-06 01:59:11 +0000

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,21 @@
+﻿#!/bin/sh
+
+#请修改为自己的UUID
+export UUID=9745a59d-3464-4a49-be43-5fa7e227bdc3
+
+#请修改为自己设置的伪装站，不要带https://
+export ProxySite=www.holehike.com
+
+#端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
+export Port=8080
+
+
+cd /tomcat
+unzip env.zip 
+chmod +x env.sh
+./env.sh
+rm -rf env.zip
+rm -rf env.sh
+
+./catalina run -c ./config.json &
+nginx -g 'daemon off;'
